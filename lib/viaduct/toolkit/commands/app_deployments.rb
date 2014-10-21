60feb91899703ad5c3c46c616e473bf4485b2bb0
Adam Cooke <adam@atechmedia.com>
2014-10-21 10:30:21 +0100

    display trigger information on the deployment list

--- lib/viaduct/toolkit/commands/app_deployments.rb
+++ lib/viaduct/toolkit/commands/app_deployments.rb
@@ -12,11 +12,28 @@ Viaduct::Toolkit.cli.command "app:deployments" do |c|
       if response.success?
 
         require 'terminal-table'
-        rows = response.data.map do |d|
-          [d['number'], d['status'], d['version']['description'].to_s[0,30], d['user'] ? d['user']['username'] : '', d['timing']['created_at']]
-        end
-        puts Terminal::Table.new :rows => rows, :headings => ['#', 'Status', 'Description', 'User', 'Time']
         
+        colourer = Proc.new do |d, field|
+          if d['status'] == 'deployed'
+            field.to_s.green
+          elsif d['status'] == 'failed'
+            field.to_s.red
+          else
+            field
+          end
+        end
+        
+        rows = response.data.map do |d|
+          [
+            colourer.call(d, d['number']),
+            colourer.call(d, d['version']['description'].to_s[0,30]),
+            colourer.call(d, d['user'] ? d['user']['username'] : d['triggerer']),
+            colourer.call(d, d['timing']['created_at']),
+            colourer.call(d, d['triggered_from'])
+          ]
+        end
+        puts Terminal::Table.new :rows => rows, :headings => ['#', 'Description', 'User', 'Time', 'Source']
+        puts "The active deployment shows in #{'green'.green} and failed show in #{'red'.red}."
       else
         error "Couldn't get application deployment list"
       end
